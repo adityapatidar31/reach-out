f16eff1b8216a6c46c92faa57b7678ced0379790
Aditya Patidar <adityapatidar.2020@gmail.com>
2025-04-09 23:38:06 +0530

    add validation in the Login Request

--- Backend/src/main/java/com/reach/out/Dto/LoginRequest.java
+++ Backend/src/main/java/com/reach/out/Dto/LoginRequest.java
@@ -1,7 +1,15 @@
 package com.reach.out.Dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public class LoginRequest {
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email")
     private String email;
+
+    @NotBlank(message = "Password is required")
     private String password;
 
     public String getEmail() {
