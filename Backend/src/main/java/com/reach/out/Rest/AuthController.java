a2af9539f1dd9358f0e09a5ac27299811058115e
Aditya Patidar <adityapatidar.2020@gmail.com>
2025-04-09 21:37:02 +0530

    create the signup route

--- Backend/src/main/java/com/reach/out/Rest/AuthController.java
+++ Backend/src/main/java/com/reach/out/Rest/AuthController.java
@@ -1,4 +1,28 @@
 package com.reach.out.Rest;
 
+import com.reach.out.Rest.Dto.SignupRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1")
 public class AuthController {
+
+    @PostMapping("/signUp")
+    public ResponseEntity<Map<String, Object>> signUpUser(@RequestBody SignupRequest signupRequest){
+        Map<String, Object> response=new HashMap<>();
+        Map<String, String> data = new HashMap<>();
+
+        data.put("name", signupRequest.getName());
+        data.put("email", signupRequest.getEmail());
+        data.put("password", signupRequest.getPassword());
+        data.put("confirmPassword", signupRequest.getConfirmPassword());
+        response.put("status","success");
+        response.put("data", data);
+
+        return  ResponseEntity.ok(response);
+    }
 }
