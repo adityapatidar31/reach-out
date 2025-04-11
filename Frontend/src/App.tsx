7f1e55c425022f8787e8b8d2e42eb188c9a77afd
Aditya Patidar <adityapatidar.2020@gmail.com>
2025-04-11 15:53:03 +0530

    create the Placeholder helppage and add imageUrl to the user

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Navbar from "./components/components/navbar/Navbar";
 import HomePage from "./components/components/pages/HomePage/HomePage";
+import HelpPage from "./components/components/pages/HelpPage/HelpPage";
 
 export const queryClient = new QueryClient();
 
@@ -30,6 +31,7 @@ function App() {
           <main className="p-4">
             <Routes>
               <Route path="/" element={<HomePage />} />
+              <Route path="/help/:id" element={<HelpPage />} />
               {/* <Route path="/signup" element={<SignupPage />} /> */}
               {/* <Route path="/login" element={<LoginPage />} /> */}
             </Routes>
