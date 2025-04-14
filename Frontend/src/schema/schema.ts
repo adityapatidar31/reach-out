6c97e63634c1b55426b64f90ee893c6ff0ebd210
Aditya Patidar <adityapatidar.2020@gmail.com>
2025-04-15 03:35:11 +0530

    add userSlice to store user if he is loged in

--- Frontend/src/schema/schema.ts
+++ Frontend/src/schema/schema.ts
@@ -26,15 +26,10 @@ export const userSchema = z.object({
   id: z.number(),
   name: z.string(),
   email: z.string().email(),
-  imageUrl: z
-    .string()
-    .nullable()
-    .transform(
-      (url) =>
-        url ??
-        "https://images.unsplash.com/photo-1511367461989-f85a21fda167?q=80&w=1931&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    ),
+  imageUrl: z.string().url().nullable().optional(),
 });
+
+export type User = z.infer<typeof userSchema>;
 
 export const detailedHelpSchema = z.object({
   id: z.number(),
--- Frontend/src/store/userSlice.ts
+++ Frontend/src/store/userSlice.ts
@@ -1,34 +1,36 @@
+import { User, userSchema } from "@/schema/schema";
 import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
 
-// Define a type for the slice state
-interface CounterState {
-  value: number;
+interface UserState {
+  user: User | null;
 }
 
-// Define the initial state using that type
-const initialState: CounterState = {
-  value: 0,
+const initialState: UserState = {
+  user: null,
 };
 
-export const counterSlice = createSlice({
-  name: "counter",
-  // `createSlice` will infer the state type from the `initialState` argument
+export const userSlice = createSlice({
+  name: "user",
   initialState,
   reducers: {
-    increment: (state) => {
-      state.value += 1;
+    addUser: (state, action: PayloadAction<User>) => {
+      const parsed = userSchema.safeParse(action.payload);
+      if (parsed.success) {
+        const user = parsed.data;
+        state.user = {
+          ...user,
+        };
+      } else {
+        console.error("Invalid user payload", parsed.error);
+      }
     },
-    decrement: (state) => {
-      state.value -= 1;
-    },
-    // Use the PayloadAction type to declare the contents of `action.payload`
-    incrementByAmount: (state, action: PayloadAction<number>) => {
-      state.value += action.payload;
+    deleteUser: (state) => {
+      state.user = null;
     },
   },
 });
 
-export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+export const { addUser, deleteUser } = userSlice.actions;
 
-export default counterSlice.reducer;
+export default userSlice.reducer;
