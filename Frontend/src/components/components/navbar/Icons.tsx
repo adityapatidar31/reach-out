c5e25c1226ea40b12e5897e0538547dc74d2e063
Aditya Patidar <adityapatidar.2020@gmail.com>
2025-04-17 18:36:46 +0530

    add background and ui of notication

--- Frontend/src/components/components/navbar/Icons.tsx
+++ Frontend/src/components/components/navbar/Icons.tsx
@@ -1,17 +1,20 @@
 import { Button } from "@/components/ui/button";
 import { BellRing, MessageSquareQuote } from "lucide-react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useCurrentUser } from "@/hooks/useCurrentUser";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
+import { cn } from "@/lib/utils";
 
 function Icons() {
   const user = useCurrentUser();
-  if (!user) return null;
+  const location = useLocation();
 
+  if (!user) return null;
+  const isActivePage = location.pathname == "/messages";
   return (
     <>
       {/* Notification Bell with Popover */}
@@ -21,17 +24,23 @@ function Icons() {
             <BellRing className="text-primary" />
           </Button>
         </PopoverTrigger>
-        <PopoverContent className="w-64 text-center">
-          <h3 className="text-lg font-semibold mb-2">Notifications</h3>
+        <PopoverContent className="w-72 p-4 rounded-xl shadow-lg text-center">
+          <h3 className="text-lg font-semibold mb-2 text-foreground">
+            Notifications
+          </h3>
           <p className="text-sm text-muted-foreground">
-            You have no new notifications.
+            You have no new notifications yet. Stay tuned!
           </p>
         </PopoverContent>
       </Popover>
 
       {/* Message Icon */}
       <Link to="/messages">
-        <Button variant="ghost" size="icon" className="cursor-pointer">
+        <Button
+          variant="ghost"
+          size="icon"
+          className={cn("cursor-pointer", isActivePage ? "bg-accent" : "")}
+        >
           <MessageSquareQuote className="text-primary" />
         </Button>
       </Link>
